8d4c6c53935ffee26c448f944b35e4c530511e89
mdacht777 <mdacht777@gmail.com>
2020-12-09 11:52:38 -0700

    commit tree 7f6176f165c66afc30833da4b202805a2c4d691e

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,14 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends:[]}, action){
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return ({...state,friends: [...state.friends,action.friend]})
+    case "REMOVE_FRIEND":
+    //   const friendID = (element) => element.id === action.id;
+    //   console.log(state.friends.findIndex(friendID));
+    const friendID = state.friends.findIndex(element => element.id === action.id);
+      return ({...state,friends: [...state.friends.slice(0, friendID),...state.friends.slice(friendID + 1)]})
+    default:
+      return state;
+  }
 }
+
