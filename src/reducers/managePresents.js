3703e305c2e9b2fdd51ba0b628e7b52105b3a3f8
mdacht777 <mdacht777@gmail.com>
2020-12-09 11:53:27 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
 export function managePresents(state, action){
+    switch (action.type) {
+    case 'INCREASE':
+        return {numberOfPresents: state.numberOfPresents + 1}
+    // case 'DECREASE_COUNT':
+    //     return {count: state.count - 1}
+    default:
+        return state;
+    }
 }
